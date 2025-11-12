c3b0457b03466285b8201755c1b173663ba84c71
JingHng <wanzytales@gmail.com>
2025-11-12 20:48:30 +0800

    Updated CORS temporarily while trying to fix deployment issues

--- api/app.js
+++ api/app.js
@@ -21,27 +21,7 @@ BigInt.prototype.toJSON = function() {
 const app = express();
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin 
-    if (!origin) return callback(null, true);
-    
-    // Allow localhost for development
-    if (origin.includes('localhost')) {
-      return callback(null, true);
-    }
-    
-    // Allow Azure domain
-    if (origin.includes('azurewebsites.net')) {
-      return callback(null, true);
-    }
-    
-    // Specific Azure domain 
-    if (origin === 'https://sdcgroup3-bwbnekdtd7h8bzg4.malaysiawest-01.azurewebsites.net') {
-      return callback(null, true);
-    }
-    
-    callback(new Error('Not allowed by CORS'));
-  },
+  origin: true, // Allow all origins temporarily
   credentials: true,
   optionsSuccessStatus: 200
 };
