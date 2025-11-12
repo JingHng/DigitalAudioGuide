e772fe45d80502d7503ae7093c6fa3f32435c64f
ruyi520520 <ruyih.24@ichat.sp.edu.sg>
2025-11-12 13:20:28 +0800

    Add getAllBadge/getUserBadge/uploadBadgeImage functions

---
+++ api/src/routes/badgeRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const badgeController = require('../controllers/badgeController');
+// const jwtMiddleware = require("../middleware/jwtMiddleware"); 
+// const { checkPermission } = require("../middleware/permissionMiddleware"); 
+// const { uploadImage } = require('../middleware/fileUploads');
+
+
+// --- PUBLIC ROUTES ---
+// Get all badges (for everyone)
+router.get('/allBadges', badgeController.getAllBadges);
+// Get badges of the logged-in user
+router.get('/userBadges', badgeController.getUserBadges);
+
+
+// // --- ADMIN-ONLY ROUTES ---
+// // Route to update an existing badge
+// router.post(
+//   "/:exhibitId/update-badge-image",
+//   jwtMiddleware.verifyAccessToken,
+//   roleMiddleware(["admin", "super_admin"]),
+//   upload.single("image"),
+//   exhibitController.updateBadgeImage
+// );
+
+module.exports = router;
